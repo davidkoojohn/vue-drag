43dc5a7f27b6937505e0c113a9c36a3964e901b0
davidkoojohn <davidkoojohn@gmail.com>
2018-06-27 11:12:33 +0800

    clear code & use scss

---
+++ src/components/Drop.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="drop-wrap">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Drop',
+  data () {
+    return {
+      msg: 'drop'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
